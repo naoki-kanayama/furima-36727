456e5a819c6137ed93468ffa9e11bd8d32350b2c
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-03 16:05:32 +0900

    登録済みユーザーに対する商品保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:item_name, :category_id, :status_id, :price, :text, :shipping_date_id, :prefecture_id, :delivery_fee_id, :user_id, :image)
+    params.require(:item).permit(:item_name, :category_id, :status_id, :price, :text, :shipping_date_id, :prefecture_id, :delivery_fee_id, :user_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,24 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
   validates :text, presence: true
-  validates :shipping_date_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_fee_id, presence: true
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   
   belongs_to :user
   has_one :record
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_date
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_fee
 end
