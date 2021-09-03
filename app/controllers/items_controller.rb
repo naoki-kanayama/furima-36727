29a6f1de32259d188a94cf165e39f8c447446ede
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-03 12:01:17 +0900

    itemsテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
-  def index
+  def new
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:)
 end
