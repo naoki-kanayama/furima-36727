5653b7f074f84c048f9adb9b54144c27c5d8d18c
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-03 12:25:53 +0900

    投稿画面作成・Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,5 +10,6 @@ class ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:)
+    params.require(:item).permit(:item_name, :category_id, :status_id, :price, :text, :shipping_date_id, :prefecture_id, :delivery_fee_id, :user_id, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :price, presence: true
+  validates :text, presence: true
+  validates :shipping_date_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_fee_id, presence: true
+  
+  belongs_to :user
+  has_one :record
+  has_one_attached :image
 end
