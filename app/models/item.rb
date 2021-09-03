26a8841bff66a7b28fc1c5398e0117b341f504b7
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-03 16:48:16 +0900

    ログイン状態による分岐機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,12 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true
+  validates :price, presence: true,
   validates :text, presence: true
   validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
   
   belongs_to :user
   has_one :record
