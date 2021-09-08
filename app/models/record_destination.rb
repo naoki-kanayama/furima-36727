3f7b3cfab582b23cd205ddfe8dc7573311a05f9f
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-08 16:30:17 +0900

    リファクタリング

--- app/models/record_destination.rb
+++ app/models/record_destination.rb
@@ -1,14 +1,16 @@
 class RecordDestination
   include ActiveModel::Model
   attr_accessor :city, :address, :building_number, :address_number, :tel_number, :record_id, :user_id, :item_id, :prefecture_id, :token
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :address_number, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-). Enter half-width" }
-  validates :tel_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid Not include hyphen(-). Enter half-width" }
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
+  with_options presence: true do
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :address
+    validates :address_number, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-). Enter half-width" }
+    validates :tel_number, format: {with: /\A\d{10,11}\z/, message: "is invalid Not include hyphen(-). Enter half-width" }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
     
     def save
       record = Record.create(user_id: user_id, item_id: item_id)
